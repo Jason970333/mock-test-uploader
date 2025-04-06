4a82514df78a1442971cd137d4431c3a7bc54efe
Jason970333 <jason9703@naver.com>
2025-04-06 15:24:29 +0900

    Delete pages/api/index.tsx

--- pages/api/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const MockTestUploader = dynamic(() => import("../components/MockTestUploader"), {
-  ssr: false,
-});
-
-export default function Home() {
-  return <MockTestUploader />;
-}
